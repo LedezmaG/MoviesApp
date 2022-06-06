bb17825efd73747a935798a45903fa3c0c80af68
Israel ledezma <israeledezma@gmail.com>
2022-06-05 23:58:45 -0500

    Schema ando resolver

---
+++ back/src/graphql/resolvers/Index.js
@@ -0,0 +1,165 @@
+const MoviesModel = require("../../models/MoviesModel");
+const sequelize = require("../../database/connection");
+const { Op } = require("sequelize");
+
+const resolvers = {
+    Query: {
+        async movies(_, { limit, offset, search } ) {
+            console.log( { limit, offset, search });
+            const clause = {
+                where: { active: true },
+            }
+            if (limit !== null && offset !== null) {
+                clause.offset = offset
+                clause.limit = limit
+            }
+            if (search) {
+                clause.where[Op.or] = [
+                    { title: { [Op.like]: `%${search}%` } }
+                ]
+            }
+            return await MoviesModel.findAll(clause)
+        },
+        async topMovies(_, { limit, offset, search } ) {
+            const clause = {
+                where: { active: true },
+                order: [
+                    ['vote_average', 'DESC'],
+                ],
+            }
+            if (limit !== null && offset !== null) {
+                clause.offset = offset
+                clause.limit = limit
+            }
+            if (search) {
+                clause.where[Op.or] = [
+                    { title: { [Op.like]: `%${search}%` } }
+                ]
+            }
+            return MoviesModel.findAll(clause)
+        },
+        async popularMovies(_, { limit, offset, search } ) {
+            const clause = {
+                where: { active: true },
+                order: [
+                    ['popularity', 'DESC'],
+                ],
+            }
+            if (limit !== null && offset !== null) {
+                clause.offset = offset
+                clause.limit = limit
+            }
+            if (search) {
+                clause.where[Op.or] = [
+                    { title: { [Op.like]: `%${search}%` } }
+                ]
+            }
+            return MoviesModel.findAll(clause)
+        },
+        async movie(_,{ id }) {
+            return MoviesModel.findOne({
+                where: {
+                    id,
+                    active: true
+                }
+            })
+        }
+    },
+    Mutation: {
+        async createMovie(
+            _,
+            { 
+                title,
+                overview,
+                poster_src,
+                release_date,
+                vote_count,
+                vote_average,
+                popularity,
+            }
+        ) {
+            const transaction = await sequelize.transaction()
+            try {
+                await MoviesModel.create(
+                    {
+                        title,
+                        overview,
+                        poster_src,
+                        release_date,
+                        vote_count,
+                        vote_average,
+                        popularity
+                    },
+                    transaction
+                )
+                await transaction.commit()
+                return true
+            } catch (error) {
+                await transaction.rollback()
+                return false
+            }
+        },
+        async updateMovie(
+            _,
+            { 
+                id,
+                title,
+                overview,
+                poster_src,
+                release_date,
+                vote_count,
+                vote_average,
+                popularity,
+            }
+        ) {
+            const transaction = await sequelize.transaction()
+            try {
+                await MoviesModel.update(
+                    {
+                        title,
+                        overview,
+                        poster_src,
+                        release_date,
+                        vote_count,
+                        vote_average,
+                        popularity
+                    },
+                    {
+                        where: { 
+                            id
+                        },
+                        transaction
+                    }
+                )
+                await transaction.commit()
+                return true
+            } catch (error) {
+                await transaction.rollback()
+                return false
+            }
+        },
+        async deleteMovie(_,{ id }) {
+            const transaction = await sequelize.transaction()
+            try {
+                await MoviesModel.update(
+                    {
+                        active: false
+                    },
+                    {
+                        where: { 
+                            id
+                        },
+                        transaction
+                    }
+                )
+                await transaction.commit()
+                return true
+            } catch (error) {
+                await transaction.rollback()
+                return false
+            }
+        }
+    }
+}
+
+module.exports = resolvers
---
+++ back/src/graphql/schemas/index.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server');
+
+const typeDefs = gql`
+    
+    type Movie {
+        id: Int
+        title: String
+        overview: String
+        poster_src: String
+        release_date: String
+        vote_count: Int
+        vote_average: Int
+        popularity: Int
+    }
+
+    type Query {
+        movies( limit: Int, offset: Int, search: String ): [Movie]
+        topMovies( limit: Int, offset: Int, search: String ): [Movie]
+        popularMovies( limit: Int, offset: Int, search: String ): [Movie]
+        movie( id: Int! ): Movie
+    }
+
+    type Mutation {
+        # Movies
+        createMovie(
+            title: String!
+            overview: String!
+            poster_src: String!
+            release_date: String!
+            vote_count: Int!
+            vote_average: Int!
+            popularity: Int!
+        ): Boolean!
+        updateMovie( 
+            id: Int!
+            title: String!
+            overview: String!
+            poster_src: String!
+            release_date: String!
+            vote_count: Int!
+            vote_average: Int!
+            popularity: Int!
+        ): Boolean!
+        deleteMovie( id: Int ): Boolean!
+    } 
+`;
+
+module.exports = typeDefs
